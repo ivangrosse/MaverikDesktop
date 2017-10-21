11d79ad5a71a10856bca78a7694aa2d95191d634
aoelvp94 <aoelvp94@gmail.com>
2017-10-21 17:47:41 -0300

    model

---
+++ MaverikDesktop/Model/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class User
+{
+    [Key]
+    public String id { get; set; }
+    [Required]
+    public String nombre { get; set; }
+    [Required]
+    public String apellido { get; set; }
+    [Required]
+    [MaxLength(15)]
+    [Index(IsUnique = true)]
+    public String usuario { get; set; }
+    [Required]
+    [MaxLength(15)]
+    public String password { get; set; }
+
+    public User()
+	{ 
+	}
+
+
+}
