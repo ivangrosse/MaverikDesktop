d6f1f57416e4548c13ddba28d3ec8174f8c57de9
Ivan <ivangrosso@gmail.com>
2017-10-21 17:56:57 -0300

    Intentando arreglar las cosas

---
+++ MaverikDesktop/Models/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaverikDesktop.Models
+{
+
+    public class User
+    {
+        [Key]
+        public String id { get; set; }
+        [Required]
+        public String nombre { get; set; }
+        [Required]
+        public String apellido { get; set; }
+        [Required]
+        [MaxLength(15)]
+        [Index(IsUnique = true)]
+        public String usuario { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public String password { get; set; }
+
+        public User()
+        {
+        }
+
+
+    }
+}
+
+
+
